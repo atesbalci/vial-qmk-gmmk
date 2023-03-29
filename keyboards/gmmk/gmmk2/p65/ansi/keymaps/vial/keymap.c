6b60fb3b0f96b359a8299d0b274498b1c6418c56
Ates Balci <atesbalci@gmail.com>
2023-03-29 11:14:23 +0300

    Set default lighting effect

--- keyboards/gmmk/gmmk2/p65/ansi/keymaps/vial/keymap.c
+++ keyboards/gmmk/gmmk2/p65/ansi/keymaps/vial/keymap.c
@@ -33,8 +33,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_BL] = LAYOUT(
   KC_ESC,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,  KC_EQL,   KC_BSPC,  KC_GRV,
   KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,  KC_RBRC,  KC_BSLS,  KC_DEL,
-  KC_CAPS,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,  KC_ENT,             KC_HOME,
-  KC_LSFT,  KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,  KC_RSFT,            KC_UP,    KC_END,
+  KC_CAPS,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,  KC_ENT,             KC_PGUP,
+  KC_LSFT,  KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,  KC_RSFT,            KC_UP,    KC_PGDN,
   KC_LCTL,  KC_LGUI,  KC_LALT,                                KC_SPC,                                 KC_RALT,  MO(1),    KC_LEFT,  KC_DOWN,  KC_RGHT),
 
   /* Keymap _FL: Function Layer
@@ -42,8 +42,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_FL] = LAYOUT(
    KC_GRV,    KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,   KC_F10,   KC_F11,   KC_F12,   _______,  KC_INS,
   _______,  _______,  RGB_VAI,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  KC_PSCR,  KC_SCRL,  KC_PAUS,   _______,  _______,
-  _______,  _______,  RGB_VAD,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  KC_MUTE,             KC_PGUP,
-  PERKEY_TOG, RGB_HUI, RGB_HUD, RGB_SPD,  RGB_SPI,  _______,  _______,  _______,  RGB_RMOD, RGB_MOD,  _______,  KC_MPLY,             KC_VOLU,  KC_PGDN,
+  _______,  _______,  RGB_VAD,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  KC_MUTE,             KC_HOME,
+  PERKEY_TOG, RGB_HUI, RGB_HUD, RGB_SPD,  RGB_SPI,  _______,  _______,  _______,  RGB_RMOD, RGB_MOD,  _______,  KC_MPLY,             KC_VOLU,  KC_END,
   _______,  _______,  _______,                                QK_BOOT,                                _______,  _______,  KC_MPRV,   KC_VOLD,  KC_MNXT)
 };
 
@@ -51,6 +51,7 @@ typedef union {
     uint32_t raw;
     struct {
         bool per_key_active;
+        bool initialized;
     };
 } user_config_t;
 
@@ -87,6 +88,12 @@ void setKeySetColor(const char *keys, uint16_t keyCount, uint16_t r, uint16_t g,
 void keyboard_post_init_user(void) {
     key_timer = timer_read();
     user_config.raw = eeconfig_read_user();
+    if (!user_config.initialized) {
+        rgb_matrix_mode(RGB_MATRIX_BAND_SAT);
+        user_config.per_key_active = true;
+        user_config.initialized = true;
+        eeconfig_update_user(user_config.raw);
+    }
 }
 
 float getBreathingEffectMultiplier(void) {    
