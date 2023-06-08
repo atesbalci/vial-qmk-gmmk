6ee69cc4958fecc7367d66b0b7cce8f0d43e6393
Ates Balci <atesbalci@gmail.com>
2023-06-08 17:29:35 +0300

    Key map change

--- keyboards/gmmk/gmmk2/p65/ansi/keymaps/vial/keymap.c
+++ keyboards/gmmk/gmmk2/p65/ansi/keymaps/vial/keymap.c
@@ -31,18 +31,18 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   /* Keymap _BL: Base Layer (Default Layer)
    */
 [_BL] = LAYOUT(
-  KC_ESC,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,  KC_EQL,   KC_BSPC,  KC_GRV,
-  KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,  KC_RBRC,  KC_BSLS,  KC_DEL,
-  KC_CAPS,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,  KC_ENT,             KC_VOLU,
-  KC_LSFT,  KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,  KC_RSFT,            KC_UP,    KC_VOLD,
+  KC_ESC,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,  KC_EQL,   KC_BSPC,  KC_VOLU,
+  KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,  KC_RBRC,  KC_BSLS,  KC_VOLD,
+  KC_CAPS,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,  KC_ENT,             KC_GRV,
+  KC_LSFT,  KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,  KC_RSFT,            KC_UP,    KC_DEL,
   KC_LCTL,  KC_LGUI,  KC_LALT,                                KC_SPC,                                 KC_RALT,  MO(1),    KC_LEFT,  KC_DOWN,  KC_RGHT),
 
   /* Keymap _FL: Function Layer
    */
 [_FL] = LAYOUT(
-   KC_GRV,    KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,   KC_F10,   KC_F11,   KC_F12,   _______,  KC_INS,
+   KC_GRV,    KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,   KC_F10,   KC_F11,   KC_F12,   _______,  _______,
   _______,  _______,  RGB_VAI,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  KC_PSCR,  KC_SCRL,  KC_PAUS,   _______,  _______,
-  _______,  _______,  RGB_VAD,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  KC_MUTE,             _______,
+  _______,  _______,  RGB_VAD,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  KC_MUTE,             KC_INS,
   PERKEY_TOG, RGB_HUI, RGB_HUD, RGB_SPD,  RGB_SPI,  RGB_RMOD, RGB_MOD,  _______,  KC_MPRV,  KC_MNXT,  KC_MPLY,  _______,             KC_PGUP,  _______,
   _______,  _______,  _______,                                QK_BOOT,                                _______,  _______,  KC_HOME,   KC_PGDN,  KC_END)
 };
@@ -67,10 +67,10 @@ const char alphabetKeys[] = {16, 17, 18, 19, 20, 21, 22, 23, 24, 25,
                              45, 46, 47, 48, 49, 50, 51};
 const uint8_t alphabetKeyCount = 26;
 
-const char miscCharKeys[] = {11, 12, 13, 14, 15, 26, 27, 28, 29, 40, 41, 42, 52, 53, 54, 61};
+const char miscCharKeys[] = {11, 12, 13, 15, 26, 27, 28, 40, 41, 42, 52, 53, 54, 61, 43, 57};
 const uint8_t miscCharKeyCount = 16;
 
-const char modKeys[] = {44, 55, 58, 59, 60, 62, 63, 43, 57};
+const char modKeys[] = {44, 55, 58, 59, 60, 62, 63, 14, 29};
 const uint8_t modKeyCount = 9;
 
 const char arrowKeys[] = {56, 64, 65, 66};
